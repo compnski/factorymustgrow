326a137847c968ea4e72b2ec7e745fb352e8466c
Jason Freidman <jason.freidman@gmail.com>
2021-07-24 00:18:41 -0700

    Initial prototype mainbus
    Holds infinite resources
    can be connected to a producer by double clicking the lane.
    double click the connector to disconnect

--- src/components/MainBusHeader.tsx
+++ src/components/MainBusHeader.tsx
@@ -1,22 +1,33 @@
 import { MainBus } from "../types";
 
 import { Icon } from "../svgIcons";
+import { GameState } from "../factoryGame";
+import { RecipeSelector } from "./RecipeSelector";
+import { SyntheticEvent, useState } from "react";
 
 export const MainBusHeader = (props: { mainBus: MainBus }) => {
-  const uniqueItems = new Set<string>();
-  for (var [laneIdx, lane] of props.mainBus.lanes) {
-    uniqueItems.add(lane.Entity);
-  }
-  return (
-    <svg className="mainBusHeader">
-      {[...uniqueItems].map((e: string): JSX.Element => Icon(e))}
+  const [showItemSelector, setShowItemSelector] = useState(false);
 
-      {[...uniqueItems].map(
-        (e: string, idx: number): JSX.Element => (
-          <use href={`#${e}`} width="32" height="32" x={32 * idx} />
-        )
-      )}
-    </svg>
+  const addLane = (evt: SyntheticEvent) => {
+    setShowItemSelector(true);
+    evt.stopPropagation();
+  };
+  const addMainBusLane = (evt: SyntheticEvent, entity: string) => {
+    props.mainBus.AddLane(entity);
+    setShowItemSelector(false);
+  };
+  return (
+    <div className="mainBusHeader" onClick={() => setShowItemSelector(false)}>
+      <div onClick={addLane} className="clickable addLaneButton">
+        Add Lane
+      </div>
+      {showItemSelector
+        ? RecipeSelector({
+            recipes: [...GameState.UnlockedRecipes],
+            onClick: addMainBusLane,
+          })
+        : null}
+    </div>
   );
 };
 
@@ -30,3 +41,17 @@ export const MainBusHeader = (props: { mainBus: MainBus }) => {
  *     </svg>
  *   );
  * }; */
+
+/* <svg className="mainBusHeader">
+		 *   <text x="12" y="30" onClick={() => console.log("foo")}>
+		 *     Add Lane
+		 *   </text>
+		 *   {[...uniqueItems].map((e: string): JSX.Element => Icon(e))}
+
+		 *   {[...uniqueItems].map(
+		 *     (e: string, idx: number): JSX.Element => (
+		 *       <use href={`#${e}`} width="16" height="16" x={500 + 32 * idx} />
+		 *     )
+		 *   )}
+		 * </svg>
+		   ); */
