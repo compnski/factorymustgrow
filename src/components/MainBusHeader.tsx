15bf42c2d6a4be1455b0bf2133e603019683b8ca
Jason Freidman <jason.freidman@gmail.com>
2021-08-07 20:20:18 -0700

    Attach MainBus to inventory

--- src/components/MainBusHeader.tsx
+++ src/components/MainBusHeader.tsx
@@ -1,11 +1,46 @@
-import { MainBus } from "../types";
+import { EntityStack, FillEntityStack, MainBus } from "../types";
 
-import { ResearchState } from "../factoryGame";
+import { GameDispatch, ResearchState } from "../factoryGame";
 import { RecipeSelector } from "./RecipeSelector";
 import { SyntheticEvent, useState } from "react";
 import { MainBusSegment } from "./MainBusSegment";
 import { MainBusConst } from "./uiConstants";
 import { availableRecipes } from "../research";
+
+const entityIconDoubleClickHandler = (
+  evt: {
+    clientX: number;
+    clientY: number;
+    ctrlKey: boolean;
+    //target: { hasOwnProperty(p: string): boolean };
+    nativeEvent: { offsetX: number; offsetY: number };
+  },
+  laneId: number,
+  stack: EntityStack
+): void => {
+  if (evt.ctrlKey) {
+    FillEntityStack(stack, 1);
+    return;
+  }
+  const clickY = evt.nativeEvent.offsetY;
+  if (clickY < 20) {
+    GameDispatch({
+      type: "TransferFromInventory",
+      entity: stack.Entity,
+      otherStackKind: "MainBus",
+      laneId,
+    });
+  }
+
+  if (clickY > 30) {
+    GameDispatch({
+      type: "TransferToInventory",
+      entity: stack.Entity,
+      otherStackKind: "MainBus",
+      laneId,
+    });
+  }
+};
 
 export const MainBusHeader = ({
   mainBus,
@@ -25,18 +60,25 @@ export const MainBusHeader = ({
     setShowItemSelector(false);
   };
   const lanes = [];
-  var idx = 0;
-  for (var [laneId, lane] of mainBus.lanes.entries()) {
-    const laneX =
-      MainBusConst.laneOffset +
-      idx * (MainBusConst.laneWidth + MainBusConst.interLaneWidth);
+  for (var entry of mainBus.lanes.entries()) {
+    const [laneId, lane] = entry;
     lanes.push(
-      <text key={laneId} className="belt-count" x={laneX} y={20}>
-        {lane.Count}
-      </text>
+      <div
+        onDoubleClick={(evt) => entityIconDoubleClickHandler(evt, laneId, lane)}
+        key={laneId}
+        className="lane-header-item-stack"
+      >
+        <div className={`icon ${lane.Entity}`} />
+        <div className="item-stack-count-text">
+          <span>{lane.Count}</span>
+        </div>
+      </div>
     );
-    idx++;
   }
+
+  /* <text key={laneId} className="belt-count" x={laneX} y={20}>
+        {lane.Count}
+        </text> */
 
   const busLaneClicked = (laneId: number) => {
     mainBus.RemoveLane(laneId);
@@ -54,9 +96,7 @@ export const MainBusHeader = ({
           })
         : null}
       <div style={{ width: 400, height: 100 }}>
-        <svg width={400} height={30}>
-          {lanes}
-        </svg>
+        <div className="lane-header-counts">{lanes}</div>
         <MainBusSegment
           segmentHeight={70}
           mainBus={mainBus}
