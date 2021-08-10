77f5ee56da1c4bcdca5a94bc3ea2e25d685a6bbe
Jason Freidman <jason@compnski.com>
2021-08-09 23:59:27 -0700

    Split out ButtonPanel for shared component

---
+++ src/components/ButtonPanel.tsx
@@ -0,0 +1,25 @@
+import { SyntheticEvent } from "react";
+
+export type ButtonPanelProps = {
+  buttons: {
+    className?: string;
+    title: string;
+    clickHandler(evt: SyntheticEvent): void;
+  }[];
+};
+
+export function ButtonPanel({ buttons }: ButtonPanelProps) {
+  return (
+    <>
+      {buttons.map(({ className, title, clickHandler }) => (
+        <div
+          key={title}
+          className={`add-producer clickable ${className || ""}`}
+          onClick={clickHandler}
+        >
+          {title}
+        </div>
+      ))}
+    </>
+  );
+}
